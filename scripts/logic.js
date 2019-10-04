52adb09d6a9e099f10a21cb116a47410a87cf447
vagrant <mikefyke@hotmail.com>
2019-10-04 14:17:07 +0000

    Tv On and Off logic

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,3 +1,14 @@
+var tvOn = false;
+
 function printImage() {
-document.getElementById("screen").src="./images/05.gif";
+    
+    if (tvOn === false){
+        document.getElementById("screen").src="./images/05.gif";
+        tvOn = true;
+        console.log(tvOn);
+    } else if (tvOn === true){
+        document.getElementById("screen").src="./images/screen.png";
+        tvOn = false;
+        console.log(tvOn);
+    }
 }
